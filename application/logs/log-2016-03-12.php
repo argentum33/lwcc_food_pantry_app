4c10a7e5491bf8e76fdb8e043049e8432c11b527
argentum33 <tausigma33@gmail.com>
2016-03-14 00:15:03 -0400

    updated logs

---
+++ application/logs/log-2016-03-12.php
@@ -0,0 +1,17 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+ERROR - 2016-03-12 18:21:20 --> Severity: Notice  --> Undefined index: user_id C:\xampp\htdocs\easy_appointments_dir\trunk\application\models\appointment_status_model.php 79
+ERROR - 2016-03-12 18:22:31 --> Severity: Notice  --> Undefined index: user_id C:\xampp\htdocs\easy_appointments_dir\trunk\application\models\appointment_status_model.php 79
+ERROR - 2016-03-12 18:30:50 --> Severity: Notice  --> Undefined index: user_id C:\xampp\htdocs\easy_appointments_dir\trunk\application\models\appointment_status_model.php 79
+ERROR - 2016-03-12 18:32:44 --> Severity: Notice  --> Undefined index: user_id C:\xampp\htdocs\easy_appointments_dir\trunk\application\models\appointment_status_model.php 79
+ERROR - 2016-03-12 18:36:33 --> Severity: Notice  --> Undefined index: user_id C:\xampp\htdocs\easy_appointments_dir\trunk\application\models\appointment_status_model.php 79
+ERROR - 2016-03-12 18:41:52 --> Severity: Notice  --> Undefined index: user_id C:\xampp\htdocs\easy_appointments_dir\trunk\application\models\appointment_status_model.php 79
+ERROR - 2016-03-12 18:42:34 --> Severity: Notice  --> Undefined index: user_id C:\xampp\htdocs\easy_appointments_dir\trunk\application\models\appointment_status_model.php 79
+ERROR - 2016-03-12 18:45:18 --> Severity: Notice  --> Undefined index: user_id C:\xampp\htdocs\easy_appointments_dir\trunk\application\models\appointment_status_model.php 79
+ERROR - 2016-03-12 18:49:04 --> Severity: Notice  --> Undefined index: user_id C:\xampp\htdocs\easy_appointments_dir\trunk\application\models\appointment_status_model.php 79
+ERROR - 2016-03-12 18:55:17 --> Severity: Notice  --> Undefined index: user_id C:\xampp\htdocs\easy_appointments_dir\trunk\application\models\appointment_status_model.php 79
+ERROR - 2016-03-12 18:55:30 --> Severity: Notice  --> Undefined index: user_id C:\xampp\htdocs\easy_appointments_dir\trunk\application\models\appointment_status_model.php 79
+ERROR - 2016-03-12 18:56:06 --> Severity: Notice  --> Undefined index: user_id C:\xampp\htdocs\easy_appointments_dir\trunk\application\models\appointment_status_model.php 79
+ERROR - 2016-03-12 18:57:23 --> Severity: Notice  --> Undefined index: user_id C:\xampp\htdocs\easy_appointments_dir\trunk\application\models\appointment_status_model.php 79
+ERROR - 2016-03-12 18:58:39 --> Severity: Notice  --> Undefined index: user_id C:\xampp\htdocs\easy_appointments_dir\trunk\application\models\appointment_status_model.php 79
+ERROR - 2016-03-12 19:00:50 --> Severity: Notice  --> Undefined index: user_id C:\xampp\htdocs\easy_appointments_dir\trunk\application\models\appointment_status_model.php 79
---
+++ application/logs/log-2016-03-14.php
@@ -0,0 +1,4 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+ERROR - 2016-03-14 03:35:40 --> Severity: Notice  --> Array to string conversion C:\xampp\htdocs\easy_appointments_dir\trunk\system\database\drivers\mysqli\mysqli_driver.php 603
+ERROR - 2016-03-14 03:35:40 --> Query error: Unknown column 'status' in 'field list'
